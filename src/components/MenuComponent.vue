7bb919dc55a5368a63150c1f9ca11d426363ec61
ItxChewy <aarongarcia1906@gmail.com>
2024-10-30 14:22:57 +0100

    as

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,31 +1,36 @@
 <template>
   <div>
     <ul id="menu">
-        <li>
-            <router-link to="/">Home</router-link>
-        </li>
-        <li>
-            <router-link to="/deportes">Deportes</router-link>
-        </li>
-        <li>
-            <router-link to="/numerodoble/7">Numero Doble Param</router-link>
-        </li>
-        <li>
-            <router-link to="/numerodoble">Numero Doble</router-link>
-        </li>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/deportes">Deportes</router-link>
+      </li>
+      <li v-for="num in numeros" :key="num">
+        <router-link :to="`/numerodoble/${num}`">Numero {{ num }}</router-link>
+      </li>
+      <li>
+        <router-link to="/numerodoble">Numero Doble</router-link>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name:"MenuComponent"
-}
+  name: "MenuComponent",
+  data() {
+    return {
+      numeros: [3, 2, 1, 67, 19],
+    };
+  },
+};
 </script>
 
 <style>
- ul#menu li{
-display: inline;
-padding: 2%;
+ul#menu li {
+  display: inline;
+  padding: 2%;
 }
 </style>
